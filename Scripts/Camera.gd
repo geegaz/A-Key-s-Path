18b63522a1403e8655e4d29988e314f2e950f459
Geegaz <geegazrd@gmail.com>
2021-06-25 22:22:13 +0200

    Started working on Camera

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -1,11 +1,24 @@
 extends Camera2D
 
+
+export(NodePath) var camera_limits_rect
+onready var _CameraLimitsRect: Control = get_node_or_null(camera_limits_rect)
+
+export(Vector2) var target
+export(int, FLAGS, "x", "y") var lock_axis = 0
+
+onready var _Tween = $Tween
+
 func _ready():
+	if _CameraLimitsRect:
+		set_camera_limits(_CameraLimitsRect)
+
+func _process(delta):
 	pass
 
 # Launches the shaking
-func shake(scale: float, time: float, intensity: float = 80.0):
-	$Shaker.shake(self, "offset", scale, time, intensity)
+#func shake(scale: float, time: float, intensity: float = 80.0):
+#	$Shaker.shake(self, "offset", scale, time, intensity)
 
 # Sets the limits of the camera based on a Control-based node
 func set_camera_limits(rect_node: Control):
